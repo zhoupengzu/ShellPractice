29bdde413d85bc790fd1abbe92fb10275491097a
zhoupengzu <zhoupengzu@maizijf.com>
2017-10-27 10:04:06 +0800

    practice

--- ShellPractice01.sh
+++ ShellPractice01.sh
@@ -5,3 +5,14 @@ echo $define_var
 
 str_length=${#define_var}
 echo "字符串长度为：${str_length}"
+sub_str=${define_var:2:3}
+echo "截取的字符串为：${sub_str}"
+# find_str=`expr index ${define_var} am`
+# echo "查找字符串:${find_str}"
+
+arr_define=("1" "2" 3)
+echo "数组里的所有值：${arr_define[*]}" #“*”可以用@号代替
+echo "数组长度：${#arr_define[*]}" #“*”可以用@号代替
+arr_define[4]="4"
+echo "数组里的所有值:${arr_define[@]}"
+echo "数组长度：${#arr_define[@]}"
